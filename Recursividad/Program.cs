18e9a49c4c9fa429e2420b4f626ea1f467f9b6b7
= <132417628+Carlosalbertomv@users.noreply.github.com>
2023-11-21 17:46:19 -0400

    F

---
+++ Recursividad/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace recursividad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(suma(5));
+            Console.ReadLine();
+
+        }
+     
+        public static int suma(int n)
+        {
+
+            if (n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n + suma(n - 1);
+            }
+
+
+        }
+
+
+    }
+}
